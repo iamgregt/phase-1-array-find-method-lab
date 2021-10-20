a8589dce035ba5493ff3a46e3d859434c0636adf
Gregory <taylor.gregory2@gmail.com>
2021-10-19 19:02:36 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+
+function superbowlWin(array) {
+     let matchedItem = array.find(function (obj){
+        return obj.result === "W";
+     })
+    if (matchedItem){
+        return matchedItem.year
+    } else{
+        return matchedItem;
+    }
+};
